2d910f383e588a2334f5a782e78619d2aeeea41e
Michael Lehmann <lehmamic@bluewin.ch>
2014-11-14 15:14:41 +0100

    Chapter 1: Scopes And Digest - Scope Phases

--- src/scope.js
+++ src/scope.js
@@ -7,6 +7,7 @@ function Scope() {
   this.$$watchers = [];
   this.$$lastDirtyWatch = null;
   this.$$asyncQueue = [];
+  this.$$phase = null;
 }
 
 Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
@@ -26,6 +27,7 @@ Scope.prototype.$digest = function() {
   var dirty;
 
   this.$$lastDirtyWatch = null;
+  this.$beginPhase('$digest');
 
   do {
     while (this.$$asyncQueue.length) {
@@ -36,9 +38,21 @@ Scope.prototype.$digest = function() {
 
     dirty = this.$$digestOnce();
     if((dirty || this.$$asyncQueue.length) && !(ttl--)) {
+      this.$clearPhase();
       throw "10 digest iterations reached";
     }
   } while(dirty || this.$$asyncQueue.length);
+  this.$clearPhase();
+};
+
+Scope.prototype.$apply = function(expr) {
+  try {
+    this.$beginPhase('$apply');
+    return this.$eval(expr);
+  } finally {
+    this.$clearPhase();
+    this.$digest();
+  }
 };
 
 Scope.prototype.$eval = function(expr, locals) {
@@ -46,15 +60,27 @@ Scope.prototype.$eval = function(expr, locals) {
 };
 
 Scope.prototype.$evalAsync = function(expr) {
-  this.$$asyncQueue.push({ scope: this, expression: expr });
+  var self = this;
+  if(!self.$$phase && !self.$$asyncQueue.length) {
+    setTimeout(function() {
+      if(self.$$asyncQueue.length) {
+        self.$digest();
+      }
+    }, 0);
+  }
+
+  self.$$asyncQueue.push({ scope: self, expression: expr });
 };
 
-Scope.prototype.$apply = function(expr) {
-  try {
-    return this.$eval(expr);
-  } finally {
-    this.$digest();
+Scope.prototype.$beginPhase = function(phase) {
+  if(this.$$phase) {
+    throw this.$$phase + ' already in progress.';
   }
+  this.$$phase = phase;
+};
+
+Scope.prototype.$clearPhase = function() {
+  this.$$phase = null;
 };
 
 Scope.prototype.$$digestOnce = function() {
